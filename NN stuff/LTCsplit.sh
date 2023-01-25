f0b8b6ff065352589b02ea3ac9cff18a7e79d211
ComputerGenie <tim@yourcomputergenie.com>
2023-01-25 12:21:59 -0600

    change API call
    chain.so now requires an API key
    changed to blockcypher

--- NN stuff/LTCsplit.sh
+++ NN stuff/LTCsplit.sh
@@ -29,8 +29,8 @@ echo "There are currently ${inpool} txes in the mempool"
 echo "LTC/USD: ${USD}"
 echo "Our fee(per byte): ${TXFEE_SATOSHI_BYTE}"
 
-UTXOs=$(curl -s https://chain.so/api/v2/get_tx_unspent/LTC/${NN_ADDRESS} | jq '.data.txs|[map(select(( (.value|tonumber) != 0.00010000)))| .[] | {txid, output_no, value,script_hex}]')
-
+#UTXOs=$(curl -s https://chain.so/api/v2/get_tx_unspent/LTC/${NN_ADDRESS}|jq '.data.txs|[map(select(( (.value|tonumber) != 0.00010000)))| .[] | {txid, output_no, value,script_hex}]')
+UTXOs=$(curl -s "https://api.blockcypher.com/v1/ltc/main/addrs/${NN_ADDRESS}?unspentOnly=true&includeScript=true" | jq '.txrefs|[map(select(( (.value|tonumber) != 0.00010000)))| .[] | {tx_hash, tx_output_n, value, script}]')
 SPLIT_VALUE=0.0001
 COINAGE_TOTAL=0
 SPLIT_VALUE_SATOSHI=$(bc <<<"${SPLIT_VALUE}*100000000")
@@ -40,11 +40,10 @@ SPLIT_TOTAL_SATOSHI=$(bc <<<"${SPLIT_VALUE}*${SPLIT_COUNT}*100000000")
 SPLIT_TOTAL_SATOSHI=("$(printf "%.0f" ${SPLIT_TOTAL_SATOSHI})")
 
 if [[ $UTXOs != "null" ]]; then
-	for txid in $(jq -r '.[].txid' <<<"${UTXOs}"); do txids+=("$txid"); done
-	for vin in $(jq -r '.[].output_no' <<<"${UTXOs}"); do vins+=("$vin"); done
+	for txid in $(jq -r '.[].tx_hash' <<<"${UTXOs}"); do txids+=("$txid"); done
+	for vin in $(jq -r '.[].tx_output_n' <<<"${UTXOs}"); do vins+=("$vin"); done
 	for amount in $(jq -r '.[].value' <<<"${UTXOs}"); do
-		amounts+=("$amount")
-		COINAGE_TOTAL=$(bc <<<"scale=8;${COINAGE_TOTAL}+${amount}")
+		COINAGE_TOTAL=$(bc <<< "scale=8;${COINAGE_TOTAL}+(${amount}*0.00000001)")
 		COINAGE_TOTAL=${COINAGE_TOTAL/#./0.}
 	done
 	COINAGE_TOTAL_SATOSHI=$(bc <<<"scale=0;${COINAGE_TOTAL}*100000000")
